ef4c07225f80492488f4a513b945afbfa2d1abc2
Tyler Howell <tshowell1993@gmail.com>
2021-12-02 18:52:33 -0500

    Update 1.0
    Latest update to BudgetMe io that will be used for final product

--- scripts/expChart.js
+++ scripts/expChart.js
@@ -1,5 +1,11 @@
-// filename expChart.js
-// setup 
+/*
+	Author: Tyler Howell
+	Date: 12/2/2021
+	Project: BudgetMe JavaScript Document
+	Filename expChart.js
+*/
+
+// setup of chart.js Bar Chart
 const data = {
   labels: ['Housing', 'Food', 'Transportation', 'Entertainment', 'Total Expenses'],
   datasets: [{
@@ -23,7 +29,7 @@ const data = {
   }]
 };
 
-// config 
+// config for chart.js Bar Chart
 const config = {
 	type: 'bar',
 	data,
@@ -41,6 +47,7 @@ const config = {
 			annotation: {
 				annotations: {
 					line1: {
+						drawTime: 'afterDraw',
 						type: 'line',
 						scaleID: 'y',
 						value: 0,
@@ -48,7 +55,7 @@ const config = {
 						borderWidth: 2,
 						label: {
 							enabled: true,
-							content: 'Monthly Income',
+							content: 'Monthly Income'
 						}
 					}
 				}
@@ -64,16 +71,44 @@ const config = {
 };
 
 
-// render init block
+// render init block for chart.js Bar Chart
 const myChart = new Chart(
   document.getElementById('myChart'),
   config
 );
 
+// Validates if the expense form is filled and has the user submit with filled data if not
+function validateForm() {
+	let iCheck = document.forms["expenses"]["income"].value;
+	let hCheck = document.forms["expenses"]["house"].value;
+	let fCheck = document.forms["expenses"]["food"].value;
+	let tCheck = document.forms["expenses"]["trans"].value;
+	let eCheck = document.forms["expenses"]["ent"].value;
+	if (iCheck == "" && hCheck == "" && fCheck == "" && tCheck == "" && eCheck == "") {
+		alert("Please fill in required fields and then click submit!")
+		return false;
+	} else if (iCheck == "") {
+		alert("Fill in your monthly income");
+		return false;
+	} else if (hCheck == "") {
+		alert("Fill in your monthly housing expenses");
+		return false;
+	} else if (fCheck == "") {
+		alert("Fill in your monthly food related expenses");
+		return false;
+	} else if (tCheck == "") {
+		alert("Fill in your monthly transportation related expenses");
+		return false;
+	} else if (eCheck == "") {
+		alert("Fill in your monthly entertainment related expenses");
+		return false;
+	}
+}
+
 // Function to update the chart with the values that are entered by the user
 // Sets the y-axis scales to a number that is 1000 more then the users income
 function updateChart(income, house, food, trans, ent) {	
-	
+	validateForm();
 	myChart.config.options.plugins.annotation.annotations.line1.value = income.value;
 	
 	// Adds the values for house, food, trans, and ent and creates a new value on the graph for total income
@@ -84,6 +119,7 @@ function updateChart(income, house, food, trans, ent) {
 	
 	var tot = (h+f+t+e); 
 	
+	// Gathers all the values from the user and creates items for the dataset data array to push into graph
 	myChart.config.options.scales.y.max = Math.round(tot + 1000);
 	myChart.config.data.datasets[0].data[0] = house.value;
 	myChart.config.data.datasets[0].data[1] = food.value;
@@ -93,15 +129,42 @@ function updateChart(income, house, food, trans, ent) {
 	myChart.update();
 }
 
+// Creates int values for the users expenses and then using the Math.max method finds the highest value and gives the user advice based on that value
+function ExpenseAlert(updateChart) {
+	var hValue = parseInt(house.value);
+	var fValue = parseInt(food.value);
+	var tValue = parseInt(trans.value);
+	var eValue = parseInt(ent.value);
+	
+	var highExpense = (Math.max(hValue, fValue, tValue, eValue));
+	
+	if (highExpense == hValue) {
+		alert("Ensure that housing expenses are no more than 30% of your gross monthly income. Your Housing expenses should be $" + (parseInt(income.value)*.3));
+	} else if (highExpense == fValue) {
+		alert("Eating out many times a week will EAT right into your budget. Spend the money at grocery stores and try cooking more meals at home.");
+	} else if (highExpense == tValue) {
+		alert("Always be shopping around for car insurance. If there is a better option, TAKE IT! \nAlso utilizing apps that give you cash back at the pump is a great way to fuel your finances.");
+	} else if (highExpense == eValue) {
+		alert("Great options to cut back on entertainment expenses include using discounts when dining out, attending amateur sporting events, attend a matinee viewing of a movie, take advantage of happy hour specials, and buying tickets in person to avoid surcharges.");
+	} else {
+		alert("No Data Entered, Please Fill Out Form and Try Again!");
+	}
+}
+
 // Function to remove all the data that the user entered then reset the graph
 function clearData(updateChart) {
-	document.getElementById("expenses").reset();
-	myChart.config.options.scales.y.max = 0;
-	myChart.config.options.plugins.annotation.annotations.line1.value = income.value;
-	
-	myChart.config.data.datasets[0].data[0] = house.value;
-	myChart.config.data.datasets[0].data[1] = food.value;
-	myChart.config.data.datasets[0].data[2] = trans.value;
-	myChart.config.data.datasets[0].data[3] = ent.value;
-	myChart.update();
+	if (income.value == "" && house.value == "" && food.value == "" && trans.value == "" && ent.value == "") {
+		alert("No form data to delete!")
+		return false;
+	} else {
+		document.getElementById("expenses").reset();
+		myChart.config.options.scales.y.max = 0;
+
+		myChart.config.data.datasets[0].data[0] = house.value;
+		myChart.config.data.datasets[0].data[1] = food.value;
+		myChart.config.data.datasets[0].data[2] = trans.value;
+		myChart.config.data.datasets[0].data[3] = ent.value;
+		myChart.update();
+		alert("Form Data Deleted!");
+	}
 }
